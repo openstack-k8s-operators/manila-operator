300bc11c20c18b86a8a37809ac06861241b22ce9
Francesco Pantano <fpantano@redhat.com>
2023-02-20 17:05:33 +0100

    Show overall Manila status in CLI
    This patch adds the Status and Message columns so we can print the
    status of the services deployed by manila-operator.
    
    Signed-off-by: Francesco Pantano <fpantano@redhat.com>

--- api/v1beta1/manila_types.go
+++ api/v1beta1/manila_types.go
@@ -125,6 +125,8 @@ type ManilaStatus struct {
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
+//+kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.conditions[0].status",description="Status"
+//+kubebuilder:printcolumn:name="Message",type="string",JSONPath=".status.conditions[0].message",description="Message"
 
 // Manila is the Schema for the manilas API
 type Manila struct {
