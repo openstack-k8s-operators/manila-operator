9f57c084fc3251a10e7a015882e29e012e6e3ded
Francesco Pantano <fpantano@redhat.com>
2023-10-09 11:35:19 +0200

    Do not run cronJobs as privileged containers
    When kolla is not required there's no need to run containers as
    privileged (especially if we run commands that are supposed to
    interact with DB).
    This change removes the root user from the Manila cronJobs pod
    and pass a SecurityContext that doesn't violate the k8s
    requirements.
    
    Signed-off-by: Francesco Pantano <fpantano@redhat.com>

--- pkg/manila/funcs.go
+++ pkg/manila/funcs.go
@@ -1,6 +1,9 @@
 package manila
 
-import "sigs.k8s.io/controller-runtime/pkg/client"
+import (
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
 
 // GetOwningManilaName - Given a ManilaAPI, ManilaScheduler, ManilaBackup or ManilaVolume
 // object, returning the parent Manila object that created it (if any)
@@ -10,6 +13,28 @@ func GetOwningManilaName(instance client.Object) string {
 			return ownerRef.Name
 		}
 	}
-
 	return ""
+}
+
+// GetManilaSecurityContext - Returns the right set of SecurityContext that
+// does not violate the k8s requirements
+func GetManilaSecurityContext() *corev1.SecurityContext {
+	falseVal := false
+	trueVal := true
+	runAsUser := int64(42429)
+	runAsGroup := int64(42429)
+	return &corev1.SecurityContext{
+		RunAsUser:                &runAsUser,
+		RunAsGroup:               &runAsGroup,
+		RunAsNonRoot:             &trueVal,
+		AllowPrivilegeEscalation: &falseVal,
+		Capabilities: &corev1.Capabilities{
+			Drop: []corev1.Capability{
+				"ALL",
+			},
+		},
+		SeccompProfile: &corev1.SeccompProfile{
+			Type: corev1.SeccompProfileTypeRuntimeDefault,
+		},
+	}
 }
