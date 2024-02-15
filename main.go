05599cfff3623bdc262c77f9a0aeb5d676a08256
Martin Schuppert <mschuppert@redhat.com>
2024-02-15 10:36:28 +0100

    Unique LeaderElectionID

--- main.go
+++ main.go
@@ -97,7 +97,7 @@ func main() {
 		},
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "c8c223a1.openstack.org",
+		LeaderElectionID:       "858862a7.openstack.org",
 		WebhookServer: webhook.NewServer(
 			webhook.Options{
 				Port:    9443,
