bd8c648466939c18e2bd43f6b62f3fafe87b9f2f
Martin Schuppert <mschuppert@redhat.com>
2025-04-16 15:37:13 +0200

    fix golangci reported issues
    Signed-off-by: Martin Schuppert <mschuppert@redhat.com>

--- controllers/manilaapi_controller.go
+++ controllers/manilaapi_controller.go
@@ -733,7 +733,7 @@ func (r *ManilaAPIReconciler) reconcileNormal(ctx context.Context, instance *man
 					condition.TLSInputReadyCondition,
 					condition.RequestedReason,
 					condition.SeverityInfo,
-					fmt.Sprintf(condition.TLSInputReadyWaitingMessage, instance.Spec.TLS.CaBundleSecretName)))
+					condition.TLSInputReadyWaitingMessage, instance.Spec.TLS.CaBundleSecretName))
 				return ctrl.Result{}, nil
 			}
 			instance.Status.Conditions.Set(condition.FalseCondition(
@@ -757,7 +757,7 @@ func (r *ManilaAPIReconciler) reconcileNormal(ctx context.Context, instance *man
 					condition.TLSInputReadyCondition,
 					condition.RequestedReason,
 					condition.SeverityInfo,
-					fmt.Sprintf(condition.TLSInputReadyWaitingMessage, err.Error())))
+					condition.TLSInputReadyWaitingMessage, err.Error()))
 				return ctrl.Result{}, nil
 			}
 			instance.Status.Conditions.Set(condition.FalseCondition(
