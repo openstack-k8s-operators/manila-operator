03dc38e40d910150cc8ffee2bd5bee80aa624b2e
Luca Miccini <lmiccini@redhat.com>
2025-06-06 12:59:52 +0200

    Enable MTLS memcached auth
    This commit allows operators to use mtls as an authentication
    method against Memcached.
    Manila controllers will detect the presence of a purposely-created mtls
    certificate (authCertSecret) and use this to configure the
    [keystone_authtoken] section accordingly.
    Additional volumes/volumemounts will be appended to each pod.
    
    Note that this commit switches from MemcachedServersWithInet to
    MemcachedServers since keystone-middleware now uses pymemcache when
    tls=true and there is no need to use "[]" to enclose the list of
    memcached servers even for ipv6.

--- pkg/manilaapi/statefulset.go
+++ pkg/manilaapi/statefulset.go
@@ -13,6 +13,7 @@ limitations under the License.
 package manilaapi
 
 import (
+	memcachedv1 "github.com/openstack-k8s-operators/infra-operator/apis/memcached/v1beta1"
 	topologyv1 "github.com/openstack-k8s-operators/infra-operator/apis/topology/v1beta1"
 	"github.com/openstack-k8s-operators/lib-common/modules/common/env"
 	"github.com/openstack-k8s-operators/lib-common/modules/common/service"
@@ -38,6 +39,7 @@ func StatefulSet(
 	labels map[string]string,
 	annotations map[string]string,
 	topology *topologyv1.Topology,
+	memcached *memcachedv1.Memcached,
 ) (*appsv1.StatefulSet, error) {
 	manilaUser := manila.ManilaUserID
 
@@ -75,6 +77,12 @@ func StatefulSet(
 	if instance.Spec.TLS.CaBundleSecretName != "" {
 		volumes = append(volumes, instance.Spec.TLS.CreateVolume())
 		volumeMounts = append(volumeMounts, instance.Spec.TLS.CreateVolumeMounts(nil)...)
+	}
+
+	// add MTLS cert if defined
+	if memcached.Status.MTLSCert != "" && instance.Spec.MemcachedInstance != nil {
+		volumes = append(volumes, memcached.CreateMTLSVolume())
+		volumeMounts = append(volumeMounts, memcached.CreateMTLSVolumeMounts(nil, nil)...)
 	}
 
 	for _, endpt := range []service.Endpoint{service.EndpointInternal, service.EndpointPublic} {
